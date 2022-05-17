b7735d5df9cab0ae1915a9825d9196497c68e071
EC2 Default User <ec2-user@ip-172-31-47-176.ec2.internal>
2022-05-17 14:16:29 +0000

    [Add]ID指定の投稿機能

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,12 @@
 class HomesController < ApplicationController
   def top
   end
+  
+  def about
+  end
+  
+  
+  
 end
+
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_073036) do
+ActiveRecord::Schema.define(version: 2022_05_17_135918) do
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "title"
+    t.string "body"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
