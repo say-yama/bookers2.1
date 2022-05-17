51af1ac9b38070b4417e41acd7cce9b72e2de557
EC2 Default User <ec2-user@ip-172-31-47-176.ec2.internal>
2022-05-17 14:39:10 +0000

    [Add]index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
   
   
   def index
+    @books =Book.all
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_135918) do
+ActiveRecord::Schema.define(version: 2022_05_17_142213) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
